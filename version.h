fe0e590b00c91c8d53eb7b8fbe7cd6ab0c46b2c7
jakub <jakub@minimalx.com>
2025-08-07 11:11:20 +0100

    update version.h generation

---
+++ version.h
@@ -0,0 +1,13 @@
+// version.h
+#ifndef VERSION_H
+#define VERSION_H
+
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 2
+#define VERSION_PATCH 4
+
+static const char VERSION_STR[]      = "v1.2.4";
+static const char VERSION_DATE_STR[] = "2025-08-07";
+static const char VERSION_AUTHOR[]   = "JakubMini";
+
+#endif // VERSION_H
